22decaa7645b3bdfc8a9867655a51bd0be9c0aa9
KrutonS <krutonpl@gmail.com>
2021-11-12 14:13:26 +0100

    basic config done

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,29 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
-  siteMetadata: {
-    title: "Gatsby Blog Demo",
-  },
-  plugins: [
-    "gatsby-plugin-postcss",
-    {
-      resolve: "gatsby-source-datocms",
-      options: {
-        apiToken: process.env.DATO_API_TOKEN,
-        environment: process.env.DATO_ENVIRONMENT,
-      },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
+	siteMetadata: {
+		title: 'Gatsby Blog Demo',
+	},
+	plugins: [
+		'gatsby-plugin-postcss',
+		{
+			resolve: 'gatsby-source-datocms',
+			options: {
+				apiToken: process.env.DATO_API_TOKEN,
+				environment: process.env.DATO_ENVIRONMENT,
+			},
+		},
+		'gatsby-plugin-sharp',
+		'gatsby-plugin-react-helmet',
+		`gatsby-plugin-image`,
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`,
 		`gatsby-plugin-transition-link`,
-		
-  ],
+		{
+			resolve: '@danbruegge/gatsby-plugin-stylelint',
+			options: {
+				files: ['**/*.{css,sass,scss}'],
+			},
+		},
+	],
 };
