dadd9988283bd79b2b457cae10085ec31263d5a3
KrutonS <krutonpl@gmail.com>
2021-11-12 14:13:26 +0100

    basic config done

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,13 @@ module.exports = {
 		es2021: true,
 	},
 	extends: [
-		'airbnb',
-		'airbnb/hooks',
 		'eslint:recommended',
 		'plugin:react/recommended',
 		'plugin:jsx-a11y/recommended',
+		'airbnb',
+		'airbnb/hooks',
+		'airbnb-typescript',
+		// "plugin:@typescript-eslint/recommended",
 		'prettier',
 	],
 	parserOptions: {
@@ -17,10 +19,23 @@ module.exports = {
 		},
 		ecmaVersion: 13,
 		sourceType: 'module',
+		project: './tsconfig.json',
 	},
-	ignorePatterns: ['^.*', '^gatsby-*'],
-	plugins: ['react', 'jsx-a11y'],
+	ignorePatterns: ['.*', 'gatsby-*', '*.css', 'commitlint.config.js'],
+	plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
 	rules: {
 		'react/jsx-filename-extension': 0,
+		'react/function-component-definition': 0,
+		'react/prop-types': 0,
+		'arrow-body-style': 0,
+		'spaced-comment': [
+			'warn',
+			'always',
+			{
+				line: {
+					markers: ['#region', '#endregion', 'region', 'endregion'],
+				},
+			},
+		],
 	},
 };
